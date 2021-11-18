ab23148feace867b646e616484e10551b44ce41c
Tomas Denemark <denemark.tomas@gmail.com>
2021-11-18 22:03:24 +0100

    Parsing extra includes

--- engine/parser.py
+++ engine/parser.py
@@ -1,5 +1,20 @@
+import json
+from functools import reduce
+
+comma_separator = (lambda x, y: str(x) + ", " + str(y))
+string_sum = (lambda x, y: str(x) + " + " + str(y))
+newline_separator = (lambda x, y: str(x) + "\n" + str(y))
+
+def parse_includes(keymap_def):
+    includes = [f"#include {file}" for file in keymap_def["extra_dependencies"]]
+    includes = reduce(newline_separator, includes)
+    return includes
+
 def buttery_parser(input_file):
-    includes = "hi"
+    with open(input_file, "r") as file:
+        keymap_def = json.load(file)
+
+    includes = parse_includes(keymap_def)
     keycodes = ""
     pseudolayers = ""
     keyboard_parameters = ""
